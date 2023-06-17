faecbc8b7a96ba055985c732b13228a3cc9a153b
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2023-06-17 12:55:44 -0500

    Updated copyright to 2023

--- src/PQDIFExplorer.Web/ElementExtensions.cs
+++ src/PQDIFExplorer.Web/ElementExtensions.cs
@@ -1,7 +1,7 @@
 ﻿//******************************************************************************************************
 //  ElementExtensions.cs - Gbtc
 //
-//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//  Copyright © 2023, Grid Protection Alliance.  All Rights Reserved.
 //
 //  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
 //  the NOTICE file distributed with this work for additional information regarding copyright ownership.
@@ -247,7 +247,8 @@ namespace PQDIFExplorer.Web
                     string format = tag?.FormatString ?? "{0:yyyy-MM-dd HH:mm:ss.fffffff}";
                     object value = element.Get(index);
                     return string.Format(format, value);
-                },
+                }
+                ,
 
                 _ => index =>
                 {
